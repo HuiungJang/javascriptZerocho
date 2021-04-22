e4035c10a5d8a53a6d72986ca5e14774fe31cf73
HuiungJang <jhw1345@gmail.com>
2021-04-22 23:33:19 +0900

    구구단 js로 만들고 React로 만들기

--- homework_workshop/src/java_workshop/workshop1/Test01.java
+++
@@ -1,7 +0,0 @@
-package java_workshop.workshop1;
-
-public class Test01 {
-    public static void main(String[] args) {
-
-    }
-}
--- jdbc/2.jdbctest/src/com/run/Main.java
+++
@@ -1,9 +0,0 @@
-package com.run;
-
-import com.member.controller.MemberController;
-
-public class Main {
-    public static void main(String[] args) {
-        new MemberController().mainMenu();
-    }
-}
